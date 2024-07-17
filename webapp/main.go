2dcd78d5041768782e03a23750bcd30780e41f12
Leonardo <rochaleo879@gmail.com>
2024-07-17 19:54:04 -0300

    feat: adds config package

--- webapp/main.go
+++ webapp/main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"webapp/src/config"
 	"webapp/src/router"
 	"webapp/src/utils"
 )
 
 func main() {
-	fmt.Println("Rodando Webapp")
+	config.Carregar()
 	utils.CarregarTemplates()
 	r := router.Gerar()
+	fmt.Println("Rodando na porta %d\n", config.Porta)
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
