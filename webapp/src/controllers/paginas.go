bb8949fbff17b986b6c36bc0cd1431e78272822d
Leonardo <rochaleo879@gmail.com>
2024-07-19 20:17:10 -0300

    feat: creating the home page

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -11,5 +11,9 @@ func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 
 func CarregarPaginaDeCadastroDeUsuarios(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
+
+func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "home.html", nil)
 
 }
