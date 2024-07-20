8c7d6321872aac1f1510730acaf5d3bfca72ac2d
Leonardo <rochaleo879@gmail.com>
2024-07-19 23:49:59 -0300

    feat: Request with Authentication to the API

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -14,6 +18,13 @@ func CarregarPaginaDeCadastroDeUsuarios(w http.ResponseWriter, r *http.Request) 
 }
 
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	fmt.Println(response.StatusCode, erro)
 	utils.ExecutarTemplate(w, "home.html", nil)
 
 }
