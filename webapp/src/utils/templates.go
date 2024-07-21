c1263c0059d0dc62ec84b847646a2077913b200a
Leonardo <rochaleo879@gmail.com>
2024-07-21 19:07:00 -0300

    feat: Navigation Bar and Templates

--- webapp/src/utils/templates.go
+++ webapp/src/utils/templates.go
@@ -9,6 +9,8 @@ var templates *template.Template
 
 func CarregarTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
+
 }
 
 func ExecutarTemplate(w http.ResponseWriter, template string, dados interface{}) {
